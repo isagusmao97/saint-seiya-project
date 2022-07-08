c0690caf9fbab955d3f48dcd708b6db639b737a6
Isabela Gusmão <92157410+isagusmao97@users.noreply.github.com>
2022-07-08 15:21:21 -0300

    Some Changes

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -7,8 +7,8 @@
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
-    <div >
-      <div class="q-pa-md">
+    <div>
+      <div class="q-pa-none" style="width:100%">
     <q-carousel
       animated
       v-model="slide"
@@ -27,8 +27,14 @@
     </q-carousel>
   </div>
     </div>
-    
-    
+    <div class="text-center text-h2 font" style="margin-top:50px">SAINT SEIYA</div>
+    <div class="text-center text-body2 text-justify text-weight-light q-pa-xl">It all started in late 1985 when the author, Masami Kurumada, started the series originally called Saint Seiya. 
+      The first appearance took place on November 19, 1985, in an advertisement on the pages of issue 51/85 of the 
+      Weekly Shonen Jump magazine (a kind of weekly magazine published by Shueisha). 
+      But it was in issue 01/86, published on December 3, 1985, that the first chapter was published. 
+      Each magazine had approximately 400 pages, with several stories (most were in black and white) and the chapters 
+      of Knights of the Zodiac were published until the year 1990.</div>
+
     <q-page-container>
       <router-view />
     </q-page-container>
