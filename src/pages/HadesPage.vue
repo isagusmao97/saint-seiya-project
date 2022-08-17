7f92b9da874a2a4182c892e561d5eb194d924461
isabelagusmao <isabelagusmao93@gmail.com>
2022-08-17 15:51:02 -0300

    atualização de dialog

--- src/pages/HadesPage.vue
+++ src/pages/HadesPage.vue
@@ -19,11 +19,158 @@
                     </q-card-section>
                 </q-card>
 
+                 <q-card class="my-card">
+                   <img src="../assets/imagens/hypnos.jpg">
+
+                    <q-card-section align="center">
+                        <q-btn flat  style="color: #ECB905; margin-top:10px" class="font" label="HYPNOS" @click="card2 = true"/>
+                    </q-card-section>
+                </q-card>
+
+                 <q-card class="my-card">
+                   <img src="../assets/imagens/thanatos.jpg">
+
+                    <q-card-section align="center">
+                        <q-btn flat  style="color: #ECB905; margin-top:10px" class="font" label="THANATOS" @click="card3 = true"/>
+                    </q-card-section>
+                </q-card>
+
+                 <q-card class="my-card">
+                   <img src="../assets/imagens/rhadamanthys.jpg">
+
+                    <q-card-section align="center">
+                        <q-btn flat  style="color: #ECB905; margin-top:10px" class="font" label="RHADAMANTHYS" @click="card4 = true"/>
+                    </q-card-section>
+                </q-card>
+
+                 <q-card class="my-card">
+                   <img src="../assets/imagens/minos.jpg">
+
+                    <q-card-section align="center">
+                        <q-btn flat  style="color: #ECB905; margin-top:10px" class="font" label="MINOS" @click="card5 = true"/>
+                    </q-card-section>
+                </q-card>
+
+                 <q-card class="my-card">
+                   <img src="../assets/imagens/aiakos.jpg">
+
+                    <q-card-section align="center">
+                        <q-btn flat  style="color: #ECB905; margin-top:10px" class="font" label="AIAKOS" @click="card6 = true"/>
+                    </q-card-section>
+                </q-card>
             </div>
-        </div>
-        
+
+            <q-dialog v-model="card1">
+                <q-card class="my-card-description">
+                    <q-card-section>
+                    <div class="items-center">
+                        <div class="text-body2 text-center font" style="margin-bottom:10px">
+                        PANDORA
+                        </div>
+                        <div class="body-text2 text-justify font">Sister to the spirit of Hades in this life. She was an ordinary girl surrounded by her loving family, 
+                        but upon being chosen by the spirit guardian gods of Hades, Thanatos and Hypnos, she was given the power to command the 108 Specters. From then on, 
+                        she became the administrator of all events in the Underworld.</div>
+                    </div>
+                    </q-card-section>
+                </q-card>
+            </q-dialog>
+
+            <q-dialog v-model="card2">
+                <q-card class="my-card-description">
+                    <q-card-section>
+                    <div class="items-center">
+                        <div class="text-body2 text-center font" style="margin-bottom:10px">
+                        HYPNOS
+                        </div>
+                        <div class="body-text2 text-justify font">Physically, Hypnos is exactly like his twin brother Thanatos, differing only in the golden color of his eyes and hair.
+                        Unlike his brother, Hypnos is shown to be quite calm and controlled, never letting himself get carried away by anger.</div>
+                    </div>
+                    </q-card-section>
+                </q-card>
+            </q-dialog>
+
+            <q-dialog v-model="card3">
+                <q-card class="my-card-description">
+                    <q-card-section>
+                    <div class="items-center">
+                        <div class="text-body2 text-center font" style="margin-bottom:10px">
+                        THANATOS
+                        </div>
+                        <div class="body-text2 text-justify font">Physically, Thanatos is exactly like his twin brother Hypnos, differing only in the silver color of his eyes and hair.
+                        Thanatos demonstrates a much more impatient and explosive personality than his brother. He cannot stand human beings, seeing no value in them except as expendable servants. 
+                        He is also overconfident of himself, enjoying "playing" with his victims before eliminating them.</div>
+                    </div>
+                    </q-card-section>
+                </q-card>
+            </q-dialog>
+
+            <q-dialog v-model="card4">
+                <q-card class="my-card-description">
+                    <q-card-section>
+                    <div class="items-center">
+                        <div class="text-body2 text-center font" style="margin-bottom:10px">
+                        RHADAMANTHYS
+                        </div>
+                        <div class="body-text2 text-justify font">Rhadamanthys is the Specter of the Celestial Star of Ferocity, bending the Wyvern Surplice. 
+                        He is one of the Three Underworld Tycoons. His actions appear to be motivated by emotional reasons, but he acts on his intuition, developed over years of 
+                        battle; sometimes it even ignores orders to follow it. Has extreme loyalty to Hades and closeness to his subordinates.</div>
+                    </div>
+                    </q-card-section>
+                </q-card>
+            </q-dialog>
+
+            <q-dialog v-model="card5">
+                <q-card class="my-card-description">
+                    <q-card-section>
+                    <div class="items-center">
+                        <div class="text-body2 text-center font" style="margin-bottom:10px">
+                        MINOS
+                        </div>
+                        <div class="body-text2 text-justify font">One of the Three Judges of the Underworld. As they have the same level of powers, he, 
+                        Radamanthys and Aiacos are companions in battle, but also rivals with each other. He is calm and rational and always calculates the next step according to the situation.
+                        Being the only one who survived of the three, he tries to stop Seiya and the Knights from heading to the Elysian Fields, but Hyoga stays to face him.</div>
+                    </div>
+                    </q-card-section>
+                </q-card>
+            </q-dialog>
+
+            <q-dialog v-model="card6">
+                <q-card class="my-card-description">
+                    <q-card-section>
+                    <div class="items-center">
+                        <div class="text-body2 text-center font" style="margin-bottom:10px">
+                        AIAKOS
+                        </div>
+                        <div class="body-text2 text-justify font">Aiacos shows some sense of companionship, such as when he intervened in the fight between Radamanthys against Kanon, 
+                        saving the Wyvern Judge from defeat. However, Aiacos mocks the poor performance of Radamanthys, insinuating that he could have lost had he not been rescued. 
+                        As one of the three Judges of Hades' troops, Aiacos is confident of his strength, even though his overconfidence in his abilities was ultimately what led to his defeat.</div>
+                    </div>
+                    </q-card-section>
+                </q-card>
+            </q-dialog>
+        </div>  
     </q-page>
 </template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      card1: ref(false),
+      card2: ref(false),
+      card3: ref(false),
+      card4: ref(false),
+      card5: ref(false),
+      card6: ref(false),
+      card7: ref(false),
+      card8: ref(false)
+      
+    }
+  }
+}
+</script>
 
 <style lang="sass" scoped>
 .my-card
--- src/pages/PoseidonPage.vue
+++ src/pages/PoseidonPage.vue
@@ -26,7 +26,7 @@
                    <img src="../assets/imagens/io.jpg">
 
                     <q-card-section align="center">
-                        <q-btn flat  style="color: #ECB905; margin-top:10px" class="font" label="IO - SCYLLA" @click="card1 = true"/>
+                        <q-btn flat  style="color: #ECB905; margin-top:10px" class="font" label="IO - SCYLLA" @click="card2 = true"/>
                     </q-card-section>
                 </q-card>
 
@@ -34,7 +34,7 @@
                    <img src="../assets/imagens/krishna.jpg">
 
                     <q-card-section align="center">
-                        <q-btn flat  style="color: #ECB905; margin-top:10px" class="font" label="KRISHNA - CHRISAOR" @click="card1 = true"/>
+                        <q-btn flat  style="color: #ECB905; margin-top:10px" class="font" label="KRISHNA - CHRISAOR" @click="card3 = true"/>
                     </q-card-section>
                 </q-card>
 
@@ -42,7 +42,7 @@
                    <img src="../assets/imagens/kasa.jpg">
 
                     <q-card-section align="center">
-                        <q-btn flat  style="color: #ECB905; margin-top:10px" class="font" label="KASA - LYMNADES" @click="card1 = true"/>
+                        <q-btn flat  style="color: #ECB905; margin-top:10px" class="font" label="KASA - LYMNADES" @click="card4 = true"/>
                     </q-card-section>
                 </q-card>
 
@@ -50,7 +50,7 @@
                    <img src="../assets/imagens/isaak.jpg">
 
                     <q-card-section align="center">
-                        <q-btn flat  style="color: #ECB905; margin-top:10px" class="font" label="ISAAK - KRAKEN" @click="card1 = true"/>
+                        <q-btn flat  style="color: #ECB905; margin-top:10px" class="font" label="ISAAK - KRAKEN" @click="card5 = true"/>
                     </q-card-section>
                 </q-card>
 
@@ -58,7 +58,7 @@
                    <img src="../assets/imagens/sorento.jpg">
 
                     <q-card-section align="center">
-                        <q-btn flat  style="color: #ECB905; margin-top:10px" class="font" label="SORENTO - SIRENE" @click="card1 = true"/>
+                        <q-btn flat  style="color: #ECB905; margin-top:10px" class="font" label="SORENTO - SIRENE" @click="card6 = true"/>
                     </q-card-section>
                 </q-card>
 
@@ -66,7 +66,7 @@
                    <img src="../assets/imagens/kanon.jpg">
 
                     <q-card-section align="center">
-                        <q-btn flat  style="color: #ECB905; margin-top:10px" class="font" label="KANON - SEADRAGON" @click="card1 = true"/>
+                        <q-btn flat  style="color: #ECB905; margin-top:10px" class="font" label="KANON - SEADRAGON" @click="card7 = true"/>
                     </q-card-section>
                 </q-card>
 
@@ -74,33 +74,151 @@
                    <img src="../assets/imagens/thetis.jpg">
 
                     <q-card-section align="center">
-                        <q-btn flat  style="color: #ECB905; margin-top:10px" class="font" label="THETIS - MERMAID" @click="card1 = true"/>
+                        <q-btn flat  style="color: #ECB905; margin-top:10px" class="font" label="THETIS - MERMAID" @click="card8 = true"/>
                     </q-card-section>
                 </q-card>
             </div>
+
+            <q-dialog v-model="card1">
+                <q-card class="my-card-description">
+                    <q-card-section>
+                    <div class="items-center">
+                        <div class="text-body2 text-center font" style="margin-bottom:10px">
+                        BIAN (SEAHORSE)
+                        </div>
+                        <div class="body-text2 text-justify font">It is the 20th-century Seahorse Marina and one of the most powerful among the seven General Marinas. 
+                        Fought against Seiya to protect the North Pacific Ocean Pillar.</div>
+                    </div>
+                    </q-card-section>
+                </q-card>
+            </q-dialog>
+
+            <q-dialog v-model="card2">
+                <q-card class="my-card-description">
+                    <q-card-section>
+                    <div class="items-center">
+                        <div class="text-body2 text-center font" style="margin-bottom:10px">
+                        IO (SCYLLA)
+                        </div>
+                        <div class="body-text2 text-justify font">One of the Marine Generals who protected the Pillar of the South Pacific Ocean. Scylla or Scylla was a 
+                        legendary sea monster with the upper half of its body the image of a beautiful virgin and, the lower half, six demonic beasts. Just like legend, 
+                        Io hides the powers of eagle, wolf, bee, serpent, bat, and bear within her Scale.</div>
+                    </div>
+                    </q-card-section>
+                </q-card>
+            </q-dialog>
+
+            <q-dialog v-model="card3">
+                <q-card class="my-card-description">
+                    <q-card-section>
+                    <div class="items-center">
+                        <div class="text-body2 text-center font" style="margin-bottom:10px">
+                        KRISHNA (CHRISAOR)
+                        </div>
+                        <div class="body-text2 text-justify font">Krishna of Chrysaor is one of the Marine Generals, protector of the Indian Ocean Pillar. 
+                        He deeply identifies with Poseidon's earth-cleansing ideal. He firmly believed that this would serve to end the dark age.</div>
+                    </div>
+                    </q-card-section>
+                </q-card>
+            </q-dialog>
+
+            <q-dialog v-model="card4">
+                <q-card class="my-card-description">
+                    <q-card-section>
+                    <div class="items-center">
+                        <div class="text-body2 text-center font" style="margin-bottom:10px">
+                        KASA (LYMNADES)
+                        </div>
+                        <div class="body-text2 text-justify font">Kasa of Lymnades is one of the Marine Generals, charged with protecting the Southern Ocean Pillar. 
+                        Its Scale represents a water monster from Greek mythology. He has the power to read a person's mind and find out who their most loved being is,
+                        imitating him not only physically but also psychologically.</div>
+                    </div>
+                    </q-card-section>
+                </q-card>
+            </q-dialog>
+
+            <q-dialog v-model="card5">
+                <q-card class="my-card-description">
+                    <q-card-section>
+                    <div class="items-center">
+                        <div class="text-body2 text-center font" style="margin-bottom:10px">
+                        ISAAK (KRAKEN)
+                        </div>
+                        <div class="body-text2 text-justify font">Isaak of Kraken is one of the Marine Generals who protected the Arctic Ocean Pillar. 
+                        He was one of Camus' disciples at the same time as Hyoga, but ends up disappearing after being caught by a strong current saving Hyoga's life. 
+                        Rescued by Poseidon, he became a Marina.</div>
+                    </div>
+                    </q-card-section>
+                </q-card>
+            </q-dialog>
+
+            <q-dialog v-model="card6">
+                <q-card class="my-card-description">
+                    <q-card-section>
+                    <div class="items-center">
+                        <div class="text-body2 text-center font" style="margin-bottom:10px">
+                        SORENTO (SIRENE)
+                        </div>
+                        <div class="body-text2 text-justify font">One of the Marine Generals. His flute deprives the enemy of his strength and his power was able to corner 
+                        even Aldebaran. He protected the South Atlantic Ocean Pillar, but was eventually defeated by Shun. Owner of a pure heart, he believed that purifying 
+                        the earth's face was the way for Poseidon to create utopia on earth.</div>
+                    </div>
+                    </q-card-section>
+                </q-card>
+            </q-dialog>
+
+            <q-dialog v-model="card7">
+                <q-card class="my-card-description">
+                    <q-card-section>
+                    <div class="items-center">
+                        <div class="text-body2 text-center font" style="margin-bottom:10px">
+                        KANON (SEADRAGON)
+                        </div>
+                        <div class="body-text2 text-justify font">Saga's younger twin brother. Worried about the evil in his heart, Saga imprisons him in the cliff dungeon of Cape Sunion. 
+                        By a fluke of fate, Kanon ends up freeing the soul of Poseidon, the Emperor of the Seas who had been sealed by Athena. From there, he passed himself off as 
+                        General Marina of Sea Dragon, commanding the other Marinas with plans to dominate both land and sea.</div>
+                    </div>
+                    </q-card-section>
+                </q-card>
+            </q-dialog>
+
+            <q-dialog v-model="card8">
+                <q-card class="my-card-description">
+                    <q-card-section>
+                    <div class="items-center">
+                        <div class="text-body2 text-center font" style="margin-bottom:10px">
+                        THETIS (MERMAID)
+                        </div>
+                        <div class="body-text2 text-justify font">She suddenly appeared in front of Julian Solo when he turned 16, in order to induce him to awaken as Emperor of the Seas. 
+                        After that, she began to work as her advisor, secretarial work on tasks such as taking messages to Athena. In the Undersea Temple, she shows all her agility to Kiki, 
+                        but ends up defeated by Shaina's "Thunder Claws", who came to bring the Libra Armor to the Knights.</div>
+                    </div>
+                    </q-card-section>
+                </q-card>
+            </q-dialog>
         </div>
     </q-page>
 </template>
 
-// <script>
-// import { ref } from 'vue'
+ <script>
+import { ref } from 'vue'
 
-// export default {
-//   setup () {
-//     return {
-//       card1: ref(false),
-//       card2: ref(false),
-//       card03: ref(false),
-//       card04: ref(false),
-//       card05: ref(false),
-//       card06: ref(false),
-//       card07: ref(false),
-//       card08: ref(false)
+export default {
+  setup () {
+    return {
+      card1: ref(false),
+      card2: ref(false),
+      card3: ref(false),
+      card4: ref(false),
+      card5: ref(false),
+      card6: ref(false),
+      card7: ref(false),
+      card8: ref(false)
       
-//     }
-//   }
-// }
-// </script>
+    }
+  }
+}
+</script>
 
 <style lang="sass" scoped>
 .my-card
